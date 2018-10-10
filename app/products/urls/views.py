7dfbad1597e0814ee899b649c65c939fb2bdb750
parkjihye <zehye.01@gmail.com>
2018-10-10 18:59:30 +0900

    products url 작성 및 수정

--- app/products/urls/views.py
+++ app/products/urls/views.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
 
-from products.views import exhibition_total_list
+from products.views import exhibition_total_list, exhibition_detail
 
+app_name = 'products'
 urlpatterns = [
-    path('', exhibition_total_list, name='exhibition')
+    path('', exhibition_total_list, name='exhibition'),
+    path('<int:pk>/', exhibition_detail, name='exhibition-detail'),
 ]
